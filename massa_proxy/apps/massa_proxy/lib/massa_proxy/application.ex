cb9055f4cc7269bde364688f8ba94f0bec345095
Adriano Santos <sleipnir@bsd.com.br>
2021-03-14 17:29:44 -0300

    Refactor. Extract function

--- massa_proxy/apps/massa_proxy/lib/massa_proxy/application.ex
+++ massa_proxy/apps/massa_proxy/lib/massa_proxy/application.ex
@@ -17,11 +17,14 @@ defmodule MassaProxy.Application do
       %Env{
         bindings: [
           {:proxy_port, "PROXY_PORT", default: 9000, map: &String.to_integer/1, required: false},
-          {:proxy_http_port, "PROXY_HTTP_PORT", default: 9001, map: &String.to_integer/1, required: false},
+          {:proxy_http_port, "PROXY_HTTP_PORT",
+           default: 9001, map: &String.to_integer/1, required: false},
           {:user_function_host, "USER_FUNCTION_HOST", default: "0.0.0.0", required: false},
-          {:user_function_port, "USER_FUNCTION_PORT", default: 8080, map: &String.to_integer/1, required: false},
+          {:user_function_port, "USER_FUNCTION_PORT",
+           default: 8080, map: &String.to_integer/1, required: false},
           {:user_function_uds_enable, "PROXY_UDS_MODE", default: false, required: false},
-          {:user_function_sock_addr, "PROXY_UDS_ADDRESS", default: "/var/run/cloudstate.sock", required: false},
+          {:user_function_sock_addr, "PROXY_UDS_ADDRESS",
+           default: "/var/run/cloudstate.sock", required: false},
           {:heartbeat_interval, "PROXY_HEARTBEAT_INTERVAL", default: 240_000, required: false}
         ]
       }
@@ -29,6 +32,10 @@ defmodule MassaProxy.Application do
 
     config = Vapor.load!(providers)
 
+    set_vars(config)
+  end
+
+  defp set_vars(config) do
     Application.put_env(:massa_proxy, :proxy_port, config.proxy_port)
     Application.put_env(:massa_proxy, :proxy_http_port, config.proxy_http_port)
     Application.put_env(:massa_proxy, :user_function_host, config.user_function_host)
