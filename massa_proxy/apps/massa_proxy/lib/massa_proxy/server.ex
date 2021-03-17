148f9f0c315148ea6c5616254088b42db9506c17
Adriano Santos <sleipnir@bsd.com.br>
2021-03-17 17:58:20 -0300

    Template adjusts

--- massa_proxy/apps/massa_proxy/lib/massa_proxy/server.ex
+++ massa_proxy/apps/massa_proxy/lib/massa_proxy/server.ex
@@ -7,8 +7,41 @@ defmodule MassaProxy.Server do
   def start(descriptors, entities) do
     descriptors
     |> compile
+
+    # TODO: Create grpc ==> entities |> generate_services |> generate_endpoint |> start_proxy
   end
 
   defp compile(descriptors) do
+  end
+
+  defp generate_services(entities) do
+    # TODO compile templates with entities infos
+    # Ex.: EEx.eval_file(
+    #         "apps/massa_proxy/priv/templates/grpc_service.ex.eex",
+    #         [
+    #            mod_name: "ShoppingCart",
+    #            name: "Com.Example.Shoppingcart.Service",
+    #            methods: ["getCart"],
+    #            handler: "Massa.EventSourced.Handler"]
+    #          ]
+    #      )
+    #
+    # Then compile string module into Elixir code
+    # And finally
+    # return the services to create grpc endpoint
+    #
+    # services
+  end
+
+  defp generate_services(services) do
+    # Ex.: EEx.eval_file(
+    #         "apps/massa_proxy/priv/templates/grpc_endpoint.ex.eex",
+    #         [
+    #            services: services.names
+    #          ]
+    #      )
+  end
+
+  defp start_proxy(args) do
   end
 end
