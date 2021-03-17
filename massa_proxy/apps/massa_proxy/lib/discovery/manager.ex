8f8ad9b891dfe34f07b87dc500fd1d691a0a907b
Adriano Santos <sleipnir@bsd.com.br>
2021-03-17 13:11:57 -0300

    Update massa_proxy/apps/massa_proxy/lib/discovery/manager.ex
    Co-authored-by: Marcel Lanz <marcellanz@n-1.ch>

--- massa_proxy/apps/massa_proxy/lib/discovery/manager.ex
+++ massa_proxy/apps/massa_proxy/lib/discovery/manager.ex
@@ -69,7 +69,7 @@ defmodule Discovery.Manager do
     Logger.debug("Registering Entity: #{inspect(entity)} ")
 
     case entity.entity_type do
-      "cloudtate.eventsourced.EventSourced" ->
+      "cloudstate.eventsourced.EventSourced" ->
         MassaProxy.EntityRegistry.register("EventSourced", [entity])
 
       _ ->
